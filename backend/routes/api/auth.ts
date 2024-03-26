3b31669597160f4eefd9b7ac208c68e258cb4f27
Hammad Majid <hammadmajid@proton.me>
2024-03-26 17:45:15 +0500

    Refactor: change auth request type to POST

--- backend/routes/api/auth.ts
+++ backend/routes/api/auth.ts
@@ -3,10 +3,10 @@ import {login, logout, register} from "../../controllers/auth";
 
 const router: Router = express.Router();
 
-router.get("/login", login);
+router.post("/login", login);
 
-router.get("/register", register);
+router.post("/register", register);
 
-router.get("/logout", logout);
+router.post("/logout", logout);
 
 export default router;
